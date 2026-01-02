73fe81642550ecac04c09883857cf7e86862dbe0
GarrafaD'ÁguaRetornável <replosivo@gmail.com>
2026-01-02 12:28:17 -0300

    Create achievementRewards.ts

---
+++ src/systems/achievementRewards.ts
@@ -0,0 +1,14 @@
+import { Unlocks } from '../types/Unlocks';
+import { unlockSystem } from './unlocks';
+import { Achievement } from '../types/Achievement';
+
+export function applyAchievementReward(
+  achievement: Achievement,
+  unlocks: Unlocks
+): Unlocks {
+  if (achievement.reward.type === 'unlock_system') {
+    return unlockSystem(unlocks, achievement.reward.value as keyof Unlocks);
+  }
+
+  return unlocks;
+}

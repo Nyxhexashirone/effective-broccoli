5751873c38693384ef2f501733e282b69aad8f4f
GarrafaD'ÁguaRetornável <replosivo@gmail.com>
2026-01-02 12:27:27 -0300

    Create autoClick.ts

---
+++ src/data/autoClick.ts
@@ -0,0 +1,22 @@
+import { Upgrade } from '../../types/Upgrade';
+
+export const autoClickUpgrades: Upgrade[] = [
+  {
+    id: 'auto_damage',
+    name: 'Auto Damage',
+    description: 'Automatically deals damage every second.',
+    maxLevel: 50,
+    cost: {
+      bronze: { mantissa: 15, exponent: 0 },
+    },
+  },
+  {
+    id: 'auto_speed',
+    name: 'Auto Speed',
+    description: 'Increases how often auto damage is applied.',
+    maxLevel: 25,
+    cost: {
+      bronze: { mantissa: 50, exponent: 0 },
+    },
+  },
+];

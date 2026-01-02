41183a613deca44bcb3d08431f31557bfcf2be29
GarrafaD'ÁguaRetornável <replosivo@gmail.com>
2026-01-02 11:15:43 -0300

    Create currencies.ts

---
+++ src/data/currencies.ts
@@ -0,0 +1,8 @@
+import { Currency } from '../types/Currency';
+
+export const currencies: Currency[] = [
+  { id: 'bronze', name: 'Bronze', tier: 0 },
+  { id: 'silver', name: 'Silver', tier: 1 },
+  { id: 'gold', name: 'Gold', tier: 2 },
+  { id: 'platinum', name: 'Platinum', tier: 3 },
+];
